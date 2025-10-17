7631359d35b2aff294891d536fa393544709e162
lupangojino0604@gmail.com <lupangojino0604@gmail.com>
2025-10-17 12:06:58 +0800

    complete program gamit ang multiple methods

--- LearningMethonInCsahrp/Program.cs
+++ LearningMethonInCsahrp/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
 
 class Program
 {
-    static int AddNum(int a, int b)
+    static void UserInfo(string name, int age)
     {
-        return a + b;
+        Console.WriteLine("\n--- User Info ---");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
     }
+    static string FavoriteLangauge(string langauge)
+    {
+        return "Your favorite programming langauge is " + langauge + "!";
+    }
+
     static void Main()
     {
-        int result = AddNum(5, 2);
-        Console.WriteLine("The sum is: " + result);
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter you favorite langauge: ");
+        string lang = Console.ReadLine();
+
+        UserInfo(name, age);
+        string message = FavoriteLangauge(lang);
+        Console.WriteLine(message);
     }
 }
