94a89020ecb14feb28ed3bb17c2f57c31b1115a1
lupangojino0604@gmail.com <lupangojino0604@gmail.com>
2025-10-17 11:54:40 +0800

    method with multiple patrameters

--- LearningMethonInCsahrp/Program.cs
+++ LearningMethonInCsahrp/Program.cs
@@ -2,13 +2,21 @@
 
 class Program
 {
-    static void Method()
+    static void Method(string name, int age)
     {
-        Console.WriteLine("Hello,  Welcome to Csharp Method Statement Programming");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);   
+
     }
 
     static void Main()
     {
-        Method();
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Method(name, age);
     }
 }
